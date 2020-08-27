58759620f7bd749614255dfc2b282e791fdfa59d
shammikabir <shamimashammi861@gmail.com>
2020-08-27 12:05:04 +0600

    File1

---
+++ solution1.java
@@ -0,0 +1,38 @@
+public class Animal {
+	public void walk() {
+	
+		System.out.println("I am walking");
+	}
+}
+
+
+	public class Bird extends Animal{
+		void fly() {
+			System.out.println("I am flying");
+		}
+		void sing()
+		{
+			System.out.println("I am singing");
+		}
+		
+
+	}
+public class Solution {
+
+	public static void main(String[] args) {
+		Bird bird = new Bird();
+		bird.walk();
+		bird.fly();
+		bird.sing();
+
+	}
+
+}
+
+	
+
+
+
+
+	
+
